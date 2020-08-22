dd20dd873527027ef8265f95b3e28917c3a48912
Víctor Oliva <olivarra1@gmail.com>
2020-08-23 00:06:24 +0200

    Getting started

---
+++ docs/examples/CharacterCounter.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Subject } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+import { bind } from "@react-rxjs/core";
+
+const textSubject = new Subject();
+const setText = (newText) => textSubject.next(newText);
+const [useText, text$] = bind(textSubject.pipe(startWith("")));
+
+function TextInput() {
+  const text = useText();
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+const [useCharCount] = bind(text$.pipe(map((v) => v.length)));
+
+function CharacterCount() {
+  const count = useCharCount();
+
+  return <>Character Count: {count}</>;
+}
+
+export default function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Introduction: ["motivation", "doc2", "doc3"],
+    Introduction: ["motivation", "gettingStarted"],
     Tutorial: ["doc1", "doc2", "doc3"],
     "API Reference": ["mdx", "doc2", "doc3"],
   },
